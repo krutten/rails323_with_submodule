b50344799db927b3c262738a2594feba593f5b0c
Kevin Rutten <Kevin@KevinRutten.com>
2012-04-12 14:00:06 -0700

    submodule test 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestSubmodules::Application.config.secret_token = '46228f7aade68dca24d9968751cba924393563c70ff8292c4a954d2ab19b71039ed58fee1547366f3a04a1cbd2793f8f6b5196ca556a7f2989b03a8110b6f1c5'
